f00001d5336ad40941393ced7f819509cede5c7c
root <root@localhost>
2024-07-28 10:26:05 +0600

    auto publish 1722140765503

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-function toggle(){
-  document.querySelector(".items").classList.toggle("hide");
-  document.querySelector("button").classList.toggle("b");
-}
+document.addEventListener('DOMContentLoaded',()=>{
+  document.querySelectorAll('button').forEach(but=>{
+    but.addEventListener('click',()=>{
+      but.closest('.box').querySelector('.items').classList.toggle('visible');
+      but.classList.toggle('r1');
+    })
+  })
+})
+
 function theme(sub,num){
   let html = `<div class="box">
   <div class="display flex">
    <h4 class="Subname">${sub} ${num} paper</h4>
-   <button onclick="toggle()">></button>
+   <button>></button>
   </div>
   <div class="items">
    <a class="i" href="#">Laws</a>
@@ -16,5 +21,6 @@ function theme(sub,num){
  </div>`
  document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html;
 }
+theme("Physics","1<sup>st</sup>")
 theme("Bangla","1<sup>st</sup>");
 theme("Chemistry","2<sup>nd</sup>");
